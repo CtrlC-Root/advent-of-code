50591ee276b12eaef8d3bf423f8aee4d5f647aae
alex <alex@ctrlc.name>
2024-12-02 17:48:44 +0000

    Simplify build module and add comments to track using a custom test runner.
    FossilOrigin-Name: 41210c165eb35be259f5961b434b1b4be0e22504772c1e6f30d63327e2dad822

--- build.zig
+++ build.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -15,7 +12,7 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // XXX
+    // Build the solution runner.
     const binary = b.addExecutable(.{
         .name = "aoc",
         .root_source_file = b.path("runner.zig"),
@@ -23,36 +20,24 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(binary);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
+    // Build step to run the solution runner.
     const run_cmd = b.addRunArtifact(binary);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
+    // Build step to run solution runner.
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
+    // Build the test runner.
+    // XXX: use a custom test runner for better output:
+    //   https://gist.github.com/karlseguin/c6bea5b35e4e8d26af6f81c22cb5d76b
+    //   https://gist.github.com/nurpax/4afcb6e4ef3f03f0d282f7c462005f12
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("runner.zig"),
         .target = target,
@@ -61,9 +46,7 @@ pub fn build(b: *std.Build) void {
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
+    // Build step to run the test runner.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }

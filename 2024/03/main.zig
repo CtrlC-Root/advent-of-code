53afe8a882b0b2832bf2d56e69bc1a6910bfd592
alex <alex@ctrlc.name>
2024-12-03 13:56:58 +0000

    Implement Day 3 part 2.
    FossilOrigin-Name: 9157fa65cab53dbfbff91c4ee05457e11d88ee2f771eb0abdea5ffbc9067fcc8

--- 2024/03/main.zig
+++ 2024/03/main.zig
@@ -2,10 +2,20 @@ const std = @import("std");
 
 const input_data = @embedFile("input");
 
-const ParseResult = struct {
-    index: usize,
+const MultiplyOperands = struct {
     a: u32,
     b: u32,
+};
+
+const Instruction = union(enum) {
+    do: void,
+    dont: void,
+    multiply: MultiplyOperands,
+};
+
+const ParseResult = struct {
+    index: usize,
+    instruction: Instruction,
 };
 
 fn parse_mul(data: []const u8) !ParseResult {
@@ -55,8 +65,12 @@ fn parse_mul(data: []const u8) !ParseResult {
 
     return .{
         .index = index + 1,
-        .a = first_value,
-        .b = second_value,
+        .instruction = Instruction{
+            .multiply = .{
+                .a = first_value,
+                .b = second_value,
+            },
+        },
     };
 }
 
@@ -64,11 +78,42 @@ test "parse_mul" {
     const data_valid = "mul(123,456)";
     const result = try parse_mul(data_valid);
     try std.testing.expectEqual(result.index, data_valid.len);
-    try std.testing.expectEqual(result.a, 123);
-    try std.testing.expectEqual(result.b, 456);
+    try std.testing.expectEqual(result.instruction, Instruction{ .multiply = .{ .a = 123, .b = 456 } });
 
     const data_values_too_large = "mul(1234,5)";
     try std.testing.expectError(error.ValueTooLarge, parse_mul(data_values_too_large));
+}
+
+fn parse_instruction(data: []const u8) !ParseResult {
+    const do_instruction = "do()";
+    if (data.len >= do_instruction.len and std.mem.eql(u8, data[0..do_instruction.len], do_instruction)) {
+        return .{
+            .index = do_instruction.len,
+            .instruction = Instruction.do,
+        };
+    }
+
+    const dont_instruction = "don't()";
+    if (data.len >= dont_instruction.len and std.mem.eql(u8, data[0..dont_instruction.len], dont_instruction)) {
+        return .{
+            .index = dont_instruction.len,
+            .instruction = Instruction.dont,
+        };
+    }
+
+    return try parse_mul(data);
+}
+
+test "parse_instruction" {
+    const data_do = "do()";
+    const result_do = try parse_instruction(data_do);
+    try std.testing.expectEqual(result_do.index, data_do.len);
+    try std.testing.expectEqual(result_do.instruction, Instruction.do);
+
+    const data_dont = "don't()";
+    const result_dont = try parse_instruction(data_dont);
+    try std.testing.expectEqual(result_dont.index, data_dont.len);
+    try std.testing.expectEqual(result_dont.instruction, Instruction.dont);
 }
 
 fn part1(allocator: std.mem.Allocator) !usize {
@@ -92,7 +137,44 @@ fn part1(allocator: std.mem.Allocator) !usize {
         };
 
         index += result.index;
-        multiply_sum += @as(usize, @intCast(result.a)) * @as(usize, @intCast(result.b));
+        switch (result.instruction) {
+            .multiply => |operands| {
+                multiply_sum += @as(usize, @intCast(operands.a)) * @as(usize, @intCast(operands.b));
+            },
+            else => unreachable,
+        }
+    }
+
+    return multiply_sum;
+}
+
+fn part2(allocator: std.mem.Allocator) !usize {
+    _ = allocator;
+
+    var multiply_sum: usize = 0;
+    var multiply_enabled: bool = true;
+
+    var index: usize = 0;
+    while (index < input_data.len) {
+        const result = parse_instruction(input_data[index..]) catch {
+            index += 1;
+            continue;
+        };
+
+        index += result.index;
+        switch (result.instruction) {
+            .multiply => |operands| {
+                if (multiply_enabled) {
+                    multiply_sum += @as(usize, @intCast(operands.a)) * @as(usize, @intCast(operands.b));
+                }
+            },
+            .do => {
+                multiply_enabled = true;
+            },
+            .dont => {
+                multiply_enabled = false;
+            }
+        }
     }
 
     return multiply_sum;
@@ -105,11 +187,14 @@ pub fn main() !void {
     const allocator = general_purpose_allocator.allocator();
 
     // part 1
-    const result = try part1(allocator);
+    const result_all = try part1(allocator);
 
-    std.debug.assert(result == 178794710);
-    std.debug.print("result: {d}\n", .{ result });
+    std.debug.assert(result_all == 178794710);
+    std.debug.print("result all: {d}\n", .{ result_all });
 
     // part 2
-    // TODO
+    const result_conditional = try part2(allocator);
+
+    std.debug.assert(result_conditional == 76729637);
+    std.debug.print("result conditional: {d}\n", .{ result_conditional });
 }
